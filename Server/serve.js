6c8a7f3465146ce58520e28c5a6669de45514621
touseefahmad <touseefahmad788@gmail.com>
2018-08-28 07:55:17 +0500

    added get by id api

--- Server/serve.js
+++ Server/serve.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todos');
@@ -28,6 +29,21 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(e);
   });
 });
+app.get('/todos/:id',(req,res)=>{
+  var id =req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send({});
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send({});
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+  //res.send(req.params);
+})
 app.listen(3000,()=>{
   console.log('started on port 3000');
 });
