7797b787b9cb30766310b0d56d95cd19a778a562
touseefahmad <touseefahmad788@gmail.com>
2018-08-29 12:25:35 +0500

    added development and production db values

--- Server/serve.js
+++ Server/serve.js
@@ -1,3 +1,15 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env === 'development'){
+  console.log(env);
+  process.env.port = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/todoAppdb'
+}else if(env === 'test'){
+  process.env.port = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/todoApptestDb'
+}
+
+
 var express = require('express');
 const _ =require('lodash');
 var bodyParser = require('body-parser');
