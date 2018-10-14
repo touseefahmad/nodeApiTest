a1726167d8f399203d4630bb76176b0f4a130f1f
touseefahmad <touseefahmad788@gmail.com>
2018-10-14 12:59:04 +0500

    configuration files added

--- Server/config/config.js
+++ Server/config/config.js
@@ -4,7 +4,7 @@ if(env === 'development' || env ==='test'){
   var config = require('./config.json');
   var envConfig = config[env];
 Object.keys(envConfig).forEach((key)=>{
-  process.env.[key] = envConfig[key];
+  process.env[key] = envConfig[key];
 });
   //console.log(config);
 }
